2581dd796f04860afac4a05e9b724660e38feee8
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2025-02-09 12:46:10 +0400

    implement doRequest method

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+	mustToken()
 
 	// tgClient = telegram.New(token)
 
@@ -13,5 +19,13 @@ func main() {
 }
 
 func mustToken() string {
+	token := flag.String("token-bot-token", "", "token for access to tg-bot")
 
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
 }
